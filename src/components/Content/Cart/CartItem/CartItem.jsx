0e8c7a571886430505423ba22025c765f0795295
RootX <12193x@gmail.com>
2020-04-19 14:27:30 +0400

    Change cart layout

--- src/components/Content/Cart/CartItem/CartItem.jsx
+++ src/components/Content/Cart/CartItem/CartItem.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import cls from "./CartItem.module.scss";
 import Img1 from "../../../../assets/images/item-1.png"
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const CartItem = () => {
     return (
@@ -13,58 +11,45 @@ const CartItem = () => {
                         <img src={Img1} alt="Img"/>
                     </div>
                     <div className={cls["item__information"]}>
-                        <div className={cls["item__title"]}>
+                        <div className={cls["item__information_title"]}>
                             Title 1
                         </div>
-                        <div className={cls["item__description"]}>
+                        <div className={cls["item__information_description"]}>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ex placeat soluta veritatis
                             voluptatum? Doloribus hic id, illo modi natus nemo non omnis qui quidem sequi, sunt tempora
                             temporibus voluptas.
                         </div>
                     </div>
                     <div className={cls["item__quantity"]}>
-                        <button className={cls["item__button-count"]}>-</button>
-                        <input className={cls["item__input"]} type="text" name="count" aria-label="count"
-                               placeholder="0" />
-                        <button className={cls["item__button-count"]}>+</button>
+                        <button className={cls["item__quantity_button-count"]}>-</button>
+                        <input className={cls["item__quantity_input"]} type="text" name="count" aria-label="count"
+                               placeholder="0"/>
+                        <button className={cls["item__quantity_button-count"]}>+</button>
                     </div>
                     <div className={cls["item__total"]}>
                         <div className={cls["item__total-price"]}>12.44$</div>
                         <div className={cls["item__total-remove"]}>
-                            <button className={cls["item__button-count"]}><FontAwesomeIcon icon={faTrash} /></button>
-                        </div>
-                    </div>
-                </div>
-                <div className={cls["item"]}>
-                    <div className={cls["item__image"]}>
-                        <img src={Img1} alt="Img"/>
-                    </div>
-                    <div className={cls["item__information"]}>
-                        <div className={cls["item__title"]}>
-                            Title 1
-                        </div>
-                        <div className={cls["item__description"]}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ex placeat soluta veritatis
-                            voluptatum? Doloribus hic id, illo modi natus nemo non omnis qui quidem sequi, sunt tempora
-                            temporibus voluptas.
-                        </div>
-                    </div>
-                    <div className={cls["item__quantity"]}>
-                            <button className={cls["item__button-count"]}>-</button>
-                            <input className={cls["item__input"]} type="text" name="count" aria-label="count"
-                                   placeholder="0" />
-                            <button className={cls["item__button-count"]}>+</button>
-                    </div>
-                    <div className={cls["item__total"]}>
-                        <div className={cls["item__total-price"]}>12.44$</div>
-                        <div className={cls["item__total-remove"]}>
-                            <button className={cls["item__button-count"]}><FontAwesomeIcon icon={faTrash} /></button>
+                            <button className={cls["item__quantity_button-count"]}>x</button>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={cls["col-2"]}>
-
+                <div className={cls["total__subtotal"]}>
+                    <div className={cls["total__subtotal_name"]}>Subtotal</div>
+                    <div className={cls["total__subtotal_cost"]}>225$</div>
+                </div>
+                <div className={cls["total__shipping"]}>
+                    <div className={cls["total__shipping_name"]}>Shipping Fee</div>
+                    <div className={cls["total__shipping_cost"]}>10$</div>
+                </div>
+                <div className={cls["total__cost"]}>
+                    <div className={cls["total__cost_name"]}>Order Total</div>
+                    <div className={cls["total__cost_cost"]}>235$</div>
+                </div>
+                <div className={cls["total__buy"]}>
+                    <button className={cls["total__buy_button-proceed"]}>Proceed to Checkout</button>
+                </div>
             </div>
         </div>
     );
